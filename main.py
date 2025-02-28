4481fb8e321789d0a05dc34ca1dd39b6b6f0eb36
thepetertessier <fpv5gr@virginia.edu>
2025-02-28 14:57:04 -0500

    incorrect but no errors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from scipy.optimize import linprog as lp
+from scipy.optimize import linprog
+import numpy as np
 
 DEBUG = False
 
@@ -6,33 +7,76 @@ def debug(msg):
     if DEBUG:
         print(msg)
 
-class Product:
-    def __init__(self, factory_index, warehouse_index, demand):
-        self.factory_index = factory_index
-        self.warehouse_index = warehouse_index
-        self.demand = demand
-        
-class Graph:
-    def __init__(self, node_count):
-        self.data = [[0]*node_count]*node_count
+def new_a(p, n):
+    return np.array([[[0]*n]*n]*p)
 
-    def set_capacity(self, src, des, capacity):
-        self.data[src][des] = capacity
+class ConstraintBuilder:
+    def __init__(self):
+        self.A = []
+        self.b = []
+
+    def add_constraint(self, a, b_i, equality=False):
+        '''Add the constraint a*x <= b_i, or == when equality is True'''
+        self.A.append(a.flatten())
+        self.b.append(b_i)
+        if equality:
+            self.A.append(-a.flatten())
+            self.b.append(-b_i)
     
-    def get_capacity(self, src, des) -> int:
-        return self.data[src][des]
+    def get_A(self):
+        return self.A
+    
+    def get_b(self):
+        return self.b
+
+def make_constraints(capacity: dict, factory: list, warehouse: list, demand: list, node_count, product_count):
+    lp = ConstraintBuilder()
+    for u, v in capacity.keys():
+        a = new_a(product_count, node_count)
+        for i in range(product_count):
+            a[i][u][v] = 1
+        lp.add_constraint(a, capacity[(u,v)])
+    
+    for i in range(product_count):
+        for w in range(node_count):
+            a = new_a(product_count, node_count)
+            for u, v in capacity.keys():
+                a[i][u][w] = 1
+                a[i][w][v] = 1
+            lp.add_constraint(a, 0, equality=True)
+        
+        a = new_a(product_count, node_count)
+        a[i][factory[i]][warehouse[i]] = 1
+        lp.add_constraint(a, demand[i], equality=True)
+    
+    return lp.get_A(), lp.get_b()
+
 
 def main():
     node_count, edge_count, product_count = [int(x) for x in input().strip().split()]
-    products: list[Product] = []
+    
+    factory = []
+    warehouse = []
+    demand = []
     for _ in range(product_count):
-        factory_index, warehouse_index, demand = [int(x) for x in input().strip().split()]
-        products.append(Product(factory_index, warehouse_index, demand))
+        factory_index, warehouse_index, demand_i = [int(x) for x in input().strip().split()]
+        factory.append(factory_index)
+        warehouse.append(warehouse_index)
+        demand.append(demand_i)
 
-    graph = Graph(node_count)
+    capacity = {}
     for _ in range(edge_count):
-        src, des, capacity = [int(x) for x in input().strip().split()]
-        graph.set_capacity(src, des, capacity)
+        src, dst, cap = [int(x) for x in input().strip().split()]
+        capacity[(src, dst)] = cap
+
+    A, b = make_constraints(capacity, factory, warehouse, demand, node_count, product_count)
+
+    # The objective function doesn't matter - we're only interested in if the constraints can be met
+    c = [0]*node_count*node_count*product_count
+
+    result = linprog(c, A_ub=A, b_ub=b)
+    print('Yes' if result.success else 'No')
+
 
 if __name__ == '__main__':
     main()
