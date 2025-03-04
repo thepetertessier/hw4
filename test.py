62d5a3303309315bc84943f1500299f45c4080eb
thepetertessier <fpv5gr@virginia.edu>
2025-03-04 16:32:27 -0500

    test double-Yes

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import subprocess
-import os
 from pathlib import Path
+from termcolor import colored
 
 path_to_main = 'main.py'
 
 input_dir = Path('input')
+header = f'{"Test Name":<20} | {"Expected":<8} | {"Actual":<8} | Result'
+print(header)
+print('-'*len(header))
 for test_file in input_dir.iterdir():
     with open(test_file, "r") as infile:
         name, expected = test_file.stem.split('-')
-        print(f'{name:<20} | Expected: {expected}')
-        subprocess.run(['python3', path_to_main], stdin=infile)
+        print(f'{name:<20} | {expected:<8} | ', end='')
+        actual = subprocess.run(['python3', path_to_main], stdin=infile, capture_output=True, text=True).stdout.strip()
+        result = colored('Passed', 'green') if actual == expected else colored('Failed', 'red')
+        print(f'{actual:<8} | {result}')
