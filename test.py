0b9797dcc3816e55cdafbe5145b0d459db5cd15d
thepetertessier <fpv5gr@virginia.edu>
2025-03-04 16:06:32 -0500

    make test suite

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import subprocess
+import os
+from pathlib import Path
 
 path_to_main = 'main.py'
 
-with open("example_input.txt", "r") as infile:
-    subprocess.run(['python3', path_to_main], stdin=infile)
+input_dir = Path('input')
+for test_file in input_dir.iterdir():
+    with open(test_file, "r") as infile:
+        name, expected = test_file.stem.split('-')
+        print(f'{name:<20} | Expected: {expected}')
+        subprocess.run(['python3', path_to_main], stdin=infile)
